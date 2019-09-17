a65f84e2a53c2d84b6e67b4f6687f627079b05bb
Bilal Khan <bilal.khan@mac-1480-bk.local>
2019-09-17 17:29:58 -0400

    Adding db users

--- ix_tutor/models.py
+++ ix_tutor/models.py
@@ -1,6 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+
+    ACTIVE = 1
+    DEACTIVE = 2
+    STATUS_CHOICES = (
+        (ACTIVE, 'Active'),
+        (DEACTIVE, 'Deactive')
+    )
+
+    STUDENT = 0
+    TUTOR = 1
+    OCCUPATION = (
+        (STUDENT, 'Student'),
+        (TUTOR, 'Tutor')
+    )
+
+    userID = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    email = models.EmailField(
+        max_length=30,
+        default=''
+    )
+    status = models.SmallIntegerField(
+        choices=STATUS_CHOICES
+    )
+    password = models.TextField(
+        max_length=255
+    )
+    isTutor = models.SmallIntegerField(
+        choices=OCCUPATION,
+        default=''
+    )
+    isStudent = models.SmallIntegerField(
+        choices=OCCUPATION,
+        default=''
+    )
+    #deptID = models.ForeignKey(
+    #    Departments
+    #)
