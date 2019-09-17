0eb6cf07daf8e246616876bfb6cbd89f9a0afd4c
Yuriy Malkov <yura345@gmail.com>
2019-09-17 13:31:22 -0400

    fixed settings.py

--- team_merky/team_merky/settings.py
+++ team_merky/team_merky/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'team_merky'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
