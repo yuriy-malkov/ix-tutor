817024ae5e099e039a3c053898d0b215a0a631d9
Thomas Hardy <thomas.hardy@indexexchange.com>
2019-09-17 16:03:52 -0400

    Initial routes (create user, get user), broken

--- backend/backend_functions.py
+++ backend/backend_functions.py
@@ -1,13 +1,7 @@
-from flask import Flask, request, Response
-from flask_mysqldb import MySQL
+from flask import Flask, request, jsonify
 
 APP = Flask(__name__)
-APP.config['MYSQL_HOST'] = 'localhost'
-APP.config['MYSQL_USER'] = 'root'
-APP.config['MYSQL_PASSWORD'] = 'root'
-APP.config['MYSQL_DB'] = 'MyDB'
-
-mysql = MySQL(APP)
+count = 4
 
 class User:
     def __init__(self, email, password):
@@ -17,6 +11,7 @@ class User:
         self.isTutor = False
         self.isStudent = False
 
+    '''
     def insert_into_db(self):
         try:
             cur = mysql.connection.cursor()
@@ -27,30 +22,71 @@ class User:
         except e:
             print(e)
             return Response(status=504)
+    '''
 
 
 
 @APP.route('/user/register', methods=['POST'])
 def create_user():
-    email = request.form['email']
-    password = request.form['password']
+    print(request.data)
+    email = request.json['email']
+    password = request.json['password']
     new_user = User(email, password)
-    new_user.insert_into_db():
+    
+    #new_user.insert_into_db():
+    
+    test_users.append({
+        userID : count,
+        email : new_user.email,
+        password : new_user.password,
+        isTutor : False,
+        isStudent : False
+    })
+    count += 1
+    #return jsonify(new_user)
+    
 
 @APP.route('/user/login', methods=['POST'])
 def login():
     email = request.form['email']
     password = request.form['password']
+    user = get_user(email, password)
+    print(user)
 
 
 def get_user(email, password):
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM Users WHERE email = %s and password = %s", (email, password))
-    data = cur.fetchall()
-    mysql.connection.commit()
-    cur.close()
-    return userID
+    email = request.form['email']
+    password = request.form['password']
+    for user in test_users:
+        if user[email] == email and user[password] == password:
+                 return jsonify(user)
+
 
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=80)
+
+
+test_users = [
+    {
+        userID : 1,
+        email : "first@gmail.com",
+        password : "password",
+        isTutor : False,
+        isStudent : False
+    },
+    {
+        userID : 2,
+        email : "second@gmail.com",
+        password : "password",
+        isTutor : True,
+        isStudent : False
+    },
+    {
+    userID : 3,
+        email : "third@gmail.com",
+        password : "password",
+        isTutor : True,
+        isStudent : True
+    }
+]
