fa50b67bab70083aee53da6f7f0ff5391f52a383
Yuriy Malkov <yura345@gmail.com>
2019-09-17 14:21:12 -0400

    somsdlf

--- team_merky/team_merky/settings.py
+++ team_merky/team_merky/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'team_merky'
+    'team_merky',
+    'ix_tutor'
 ]
 
 MIDDLEWARE = [
